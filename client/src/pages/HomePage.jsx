e8e4028b5a993deb0de1dfd2728e1742393e4597
anuj9631 <anuj98426@gmail.com>
2025-09-16 15:19:10 +0530

    Creating components

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
+import ChatContainer from '../components/ChatContainer'
+import RightSidebar from '../components/RightSidebar'
 
 const HomePage = () => {
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
-      
+      <div className=''>
+     <Sidebar />
+     <ChatContainer/>
+     <RightSidebar />
+      </div>
     </div>
   )
 }
