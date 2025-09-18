452dcccc2a45007e65001b142f9a9ec764ded914
anuj9631 <anuj98426@gmail.com>
2025-09-18 16:18:59 +0530

    online offline status

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -27,8 +27,14 @@ const Sidebar = ({selectedUser,setSelectedUser}) => {
         {userDummyData.map((user,index)=> (
           <div> 
             <img src={user?.profilePic || assets.avatar_icon} alt=""className='w-[35px] aspect-[1/1] rounded-full'/>
-            <div>
-              
+            <div className='flex flex-col leading-5'>
+              <p>{user.fullName}</p>
+              {
+                index < 3
+                ? <span className='text-green-400 text-xs'>Online</span>
+                :
+                <span className='text-neutral-400 text-xs'>Offline</span>
+              }
             </div>
           </div>
         )) }
