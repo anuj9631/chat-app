d6c4a4061484122d46f60e357bb84153dca9b24b
anuj9631 <anuj98426@gmail.com>
2025-09-19 17:45:37 +0530

    sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -25,7 +25,7 @@ const Sidebar = ({selectedUser,setSelectedUser}) => {
       </div>
       <div className='flex flex-col'>
         {userDummyData.map((user,index)=> (
-          <div> 
+          <div className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${selectedUser ?._id === user._id && 'bg-[#282142]/50' }`}> 
             <img src={user?.profilePic || assets.avatar_icon} alt=""className='w-[35px] aspect-[1/1] rounded-full'/>
             <div className='flex flex-col leading-5'>
               <p>{user.fullName}</p>
@@ -36,6 +36,9 @@ const Sidebar = ({selectedUser,setSelectedUser}) => {
                 <span className='text-neutral-400 text-xs'>Offline</span>
               }
             </div>
+            {
+              index > 2 && <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50'>{index}</p>
+            }
           </div>
         )) }
       </div>
