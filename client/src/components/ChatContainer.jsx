840878887b3a4a9a68e1b093e421eaa19acf67b0
anuj9631 <anuj98426@gmail.com>
2025-09-20 17:43:26 +0530

    sidebar

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import assets from '../assets/assets'
 
 const ChatContainer = () => {
   return (
-    <div><h1>ChatContainer</h1></div>
+    <div>
+      <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500'>
+       <img src={assets.profile_martin} alt="" className='w-8 rounded-full' />
+       <p className='flex-1 text-lg text-white flex items-center gap-2'>Martin Johnson
+
+        <span className='w-2 h-2 rounded-full bg-green-500'></span>
+       </p>
+      </div>
+    </div>
   )
 }
 
