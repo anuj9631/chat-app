9df79b197efaf467a263f2d25931714c5ef0862d
anuj9631 <anuj98426@gmail.com>
2025-09-25 16:09:08 +0530

    Login sign up switch

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -42,6 +42,10 @@ const LoginPage = () => {
              rows={4} className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500' placeholder='provide a short bio...' required></textarea>
           )
          }
+
+         <button  type='submit' className='py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer'>
+          {currState === "Sign up" ? "Create Account" : "Login Now"}
+         </button>
       </form>
 
     </div>
