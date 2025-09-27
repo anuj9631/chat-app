20d5f554aa314f12fbcc5fb4dafacc868da5ebca
anuj9631 <anuj98426@gmail.com>
2025-09-27 20:38:45 +0530

    Issue fiexed

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -7,30 +7,35 @@ const LoginPage = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [bio, setBio] = useState("")
+  const [isDataSubmitted, setIsDataSubmitted] = useState(false)
 
   return (
     <div className="min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl">
       
       {/* ------Left------ */}
-      <img src={assets.logo_big} alt="" className="w-[min(30vw,250px)]" />
+      <img src={assets.logo_big} alt="Logo" className="w-[min(30vw,250px)]" />
       
       {/* ------Right------ */}
       <form 
         action="" 
+        onSubmit={onSubmitHandler}
         className="border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg"
       >
         <h2 className="font-medium text-2xl flex justify-between items-center">
           {currState}
-          <img 
-            src={assets.arrow_icon} 
-            alt="" 
-            className="w-5 cursor-pointer" 
-            onClick={() => setCurrState(currState === "Sign up" ? "Login" : "Sign up")} 
-          />
+
+          {isDataSubmitted && (  
+            <img 
+              src={assets.arrow_icon} 
+              alt="back" 
+              className="w-5 cursor-pointer" 
+              onClick={() => setIsDataSubmitted(false)} 
+            />
+          )}
         </h2>
 
         {/* Sign Up fields */}
-        {currState === "Sign up" && (
+        {currState === "Sign up" && !isDataSubmitted && (
           <>
             <input 
               onChange={(e) => setFullName(e.target.value)} 
@@ -59,6 +64,18 @@ const LoginPage = () => {
               className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500" 
             />
 
+            <button 
+              type="button"
+              onClick={() => setIsDataSubmitted(true)}
+              className="py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer"
+            >
+              Next
+            </button>
+          </>
+        )}
+
+        {currState === "Sign up" && isDataSubmitted && (
+          <>
             <textarea 
               onChange={(e) => setBio(e.target.value)} 
               value={bio}
@@ -67,6 +84,13 @@ const LoginPage = () => {
               placeholder="Provide a short bio..." 
               required
             ></textarea>
+
+            <button  
+              type="submit" 
+              className="py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer"
+            >
+              Create Account
+            </button>
           </>
         )}
 
@@ -90,29 +114,44 @@ const LoginPage = () => {
               required 
               className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500" 
             />
+
+            <button  
+              type="submit" 
+              className="py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer"
+            >
+              Login Now
+            </button>
           </>
         )}
 
-        <button  
-          type="submit" 
-          className="py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer"
-        >
-          {currState === "Sign up" ? "Create Account" : "Login Now"}
-        </button>
-
+        {/* Terms & Switch */}
         <div className="flex items-center gap-2 text-sm text-gray-500">
           <input type="checkbox" />
           <p>Agree to the terms of use & privacy policy.</p>
         </div>
 
         <div className='flex flex-col gap-2'>
-        {currState === 'Sign up' ? (
-          <p className='text-sm text-gray-600'>Already have an account?<span onClick={()=>{setCurrState("Login"); setIsDataSubmitted(false) }} className='font-medium text-violet-500 cursor-pointer'>Login here</span></p>
-        ) : (
-          <p className='text-sm text-gray-600'>Create an account <span 
-          onClick={()=>{setCurrState("Sign up")}}
-          className='font-medium text-violet-500 cursor-pointer'>Click here</span></p>
-        )}
+          {currState === 'Sign up' ? (
+            <p className='text-sm text-gray-600'>
+              Already have an account? 
+              <span 
+                onClick={() => { setCurrState("Login"); setIsDataSubmitted(false) }} 
+                className='font-medium text-violet-500 cursor-pointer'
+              >
+                Login here
+              </span>
+            </p>
+          ) : (
+            <p className='text-sm text-gray-600'>
+              Create an account 
+              <span 
+                onClick={() => { setCurrState("Sign up"); setIsDataSubmitted(false) }}
+                className='font-medium text-violet-500 cursor-pointer'
+              >
+                Click here
+              </span>
+            </p>
+          )}
         </div>
       </form>
     </div>
@@ -120,3 +159,4 @@ const LoginPage = () => {
 }
 
 export default LoginPage
+
