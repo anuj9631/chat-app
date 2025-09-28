d6f7e50585770333e095a59ef6a2d97f0d339795
anuj9631 <anuj98426@gmail.com>
2025-09-28 11:50:27 +0530

    profile page

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import assets from '../assets/assets'
 
 const ProfilePage = () => {
   return (
-    <div>
-      <h1>Profile Page</h1>
+    <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
+      <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
+        <form className='flex flex-col gap-5 p-10 flex-1'>
+          <h3 className='text-lg'>Profile Details</h3>
+          <label htmlFor="avatar" className='flex items-center gap-3 cursor-pointer'>
+            <input type="file" id='avatar' accept='.png, .jpeg, .jpg' hidden />
+            <img src={""} alt="" />
+          </label>
+        </form>
+        <img src="" alt="" />
+      </div>
     </div>
   )
 }
