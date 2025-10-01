53e692100d8a8f3e960e98decbdc265b656a0dbb
anuj9631 <anuj98426@gmail.com>
2025-10-01 18:29:55 +0530

    DB connected with server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config"
 import cors from "cors";
 import http from "http"
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const server = http.createServer(app)
@@ -12,6 +13,9 @@ app.use(express.json({limit: "4"}));
 app.use(cors())
 
 app.use("/api/status", (req, res)=> res.send("Server is Live"));
+
+//connect to mongodb
+await connectDB();
 
 const PORT = process.env.PORT || 5000;
 
