1dc8c2b542bcdc06f7c74af54a10274317deeac8
anuj9631 <anuj98426@gmail.com>
2025-10-05 11:10:50 +0530

    user controller login checks

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,27 @@
 //Sign up a new user
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+export const signup = async (req,res) => {
+  const {fullName, email, password, bio } = req.body;
+  try {
+    if (!fullname || !email || !password || bio)
+    {
+      return res.json({success: false, message: "Missing Details"})
+    }
+    const user = await User.findOne({email});
 
-export const signup = async (params) => {
-  
+    if (user){
+      return res.json({success: false, message: "Account already exists"})
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newuser = await User.create({
+      fullName, email, password, hashedPassword, bio
+    })
+
+    const token = 
+  } catch (error) {
+    
+  }
 }
