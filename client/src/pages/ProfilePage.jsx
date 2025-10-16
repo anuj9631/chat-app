01ee227b1fa6bd24211c8207148e06c840af516e
anuj9631 <anuj98426@gmail.com>
2025-10-16 16:26:49 +0530

    User can update his/her prfofile

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
 import {useNavigate} from 'react-router-dom'
 import assets from '../assets/assets'
+import { useContext } from 'react'
+import { AuthContext } from '../../context/AuthContext'
 
 const ProfilePage = () => {
+
+  const {authUser, updateProfile} = useContext(AuthContext)
 
 const [selectedImg, setSelectedImg] = useState(null)
 const navigate = useNavigate();
@@ -11,7 +15,18 @@ const [bio, setBio] = useState("Hi Everyone, I am Using QuickChat")
 
 const handleSubmit = async (e) => {
   e.preventDefault();
-  navigate('/')
+  if(!selectedImg){
+    await updateProfile({fullName: name, bio})
+    navigate('/')
+    return;
+  }
+  const reader = new FileRender();
+  reader.readAsDataURL(selectedImg);
+  reader.onload = async () =>{
+    const base64Image = reader.result;
+    await updateProfile({profilePic: base64Image, fullName: name, bio})
+     navigate('/');
+  }
 }
 
   return (
