a0e57008e3c95df76f6123ff3aa3968c4da808f5
anuj9631 <anuj98426@gmail.com>
2025-10-18 16:54:03 +0530

    created diffrent state to handle diffrent functions

--- client/context/ChatContext.jsx
+++ client/context/ChatContext.jsx
@@ -1,13 +1,18 @@
-import { Children, createContext } from "react";
+import { Children, createContext, useState } from "react";
 
 export const ChatContext = createContext();
 
 export const ChatProvider = ({children})=>{
 
+  const {messages, setMessages} = useState([]);
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null)
+  const [unseenMessages, setUnseenMessages] = useState({})
+  
   const value ={
-    
+
   }
-  return (<ChatContext.Provider value={}>
+  return (<ChatContext.Provider value={value}>
   {children}
   </ChatContext.Provider>)
 }
