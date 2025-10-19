1b332abc9ce024c1b96b248c63ff1cd908271b0f
anuj9631 <anuj98426@gmail.com>
2025-10-19 13:02:38 +0530

    function to send message to selected user and function to subscribe to messages for selected user

--- client/context/ChatContext.jsx
+++ client/context/ChatContext.jsx
@@ -41,6 +41,38 @@ const getMessages = async (userId) => {
   }
 }
 
+//function to send message to selected user
+const sendMessage = async (messageData) => {
+ try {
+  const {data} = await axios.post(`/api/messages/send/${selectedUser._id}`,messageData)
+  if(data.success){
+    setMessages((prevMessages)=>[...prevMessages, data.newMessage])
+  }else{
+    toast.error(data.message)
+  }
+ } catch (error) {
+  toast.error(error.message) 
+ } 
+}
+
+//function to subscribe to messages for selected user 
+
+const subscribeToMessages = async () => {
+  if(!socket) return;
+
+  socket.on("newMessage", (newMessage)=>{
+    if(selectedUser && newMessage.senderId ===   selectedUser._id){
+      newMessage.seen = true;
+      setMessages((prevMessages)=>[...prevMessages, newMessage]);
+      axios.put(`/api/messages/mark/${newMessage._id}`);
+    }else{
+      setUnseenMessages((prevUnseenMessages)=>({
+           ...prevUnseenMessages, [newMessage.senderId] : prevUnseenMessages[newMessage.senderId] ? prevUnseenMessages[newMessage.senderId] + 1 : 1
+      }))
+    }
+  })
+}
+
   const value ={
 
   }
