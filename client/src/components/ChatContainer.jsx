68809d11b18fa2af39ce93e0a790d4156ca25c36
anuj9631 <anuj98426@gmail.com>
2025-10-19 13:41:03 +0530

    handle mesaage function for users and removed props from home it will be handled by states function

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,10 +1,25 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import assets, { messagesDummyData } from '../assets/assets'
 import { formatMessageTime } from '../lib/utils'
+import { ChatContext } from '../../context/ChatContext'
+import { AuthContext } from '../../context/AuthContext'
 
-const ChatContainer = ({selectedUser, setSelectedUser}) => {
+const ChatContainer = () => {
+
+  const { messages, selectedUser, setSelectedUser, sendMessage, getMessages} = useContext(ChatContext)
+  const { authUser, onlineUsers} = useContext(AuthContext)
 
   const scrollEnd = useRef()
+
+  const [input, setInput] = useState('');
+
+  //handle sending meassge
+  const handleSendMessage = async (e) => {
+    e.preventDefault();
+    if(input.trim() == "") return null;
+    await sendMessage({text: input.trim()});
+    setInput("")
+  }
 
   useEffect(()=>{
     if(scrollEnd.current){
@@ -44,13 +59,16 @@ const ChatContainer = ({selectedUser, setSelectedUser}) => {
         {/* chat box*/}
          <div className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
              <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full'>
-               <input type="text" placeholder='Send a Message' className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400 '/>
+               <input
+               onChange={(e)=> setInput(e.target.value)} value={input}onKeyDown={(e) => e.key === "Enter" ? handleSendMessage(e) : null}
+               
+               type="text" placeholder='Send a Message' className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400 '/>
                <input type="file" id='image' accept='image/jpg, image/jpeg' hidden/>
                <label htmlFor="image">
                 <img src={assets.gallery_icon} alt="" className='w-5 mr-2 cursor-pointer' />
                </label>
              </div>
-             <img src={assets.send_button} alt="" className='w-7 cursor-pointer' />
+             <img onClick={handleSendMessage} src={assets.send_button} alt="" className='w-7 cursor-pointer' />
          </div>
 
       </div>
